280d1acee64ac671625e041cf339a4d8079d0927
uo250711 <uo250711@uniovi.es>
2016-04-03 14:43:57 +0200

    tabs

--- NumeroDeMuertos.asm
+++ NumeroDeMuertos.asm
@@ -2,56 +2,56 @@
 .model flat, C
 .code
 
-calculaNMuertosASM PROC	
+calculaNMuertosASM PROC 
 push ebp
-mov ebp, esp				; Prologo 
-sub esp, 4					; Reserva de espacio para variable local int nMuertos
+mov ebp, esp                ; Prologo 
+sub esp, 4                  ; Reserva de espacio para variable local int nMuertos
 
 push ecx 
 push edi 
 push edx
-push esi 					; Salvaguarda de registros 
+push esi                    ; Salvaguarda de registros 
 
 ; Signatura del procedimiento (parámetros formales)
-mov ecx, [ebp + 8]			; Acceso al tercer parametro propuesta[]
-mov edi, [ebp + 12]			; Acceso al primer parametro solucion[]
-mov esi, [ebp + 16]			; Acceso al primer parametro nElementos
+mov ecx, [ebp + 8]          ; Acceso al tercer parametro propuesta[]
+mov edi, [ebp + 12]         ; Acceso al primer parametro solucion[]
+mov esi, [ebp + 16]         ; Acceso al primer parametro nElementos
 
 ; Cuerpo del procedimiento 
-mov DWORD PTR [ebp - 4], 0	; nMuertos = 0
+mov DWORD PTR [ebp - 4], 0  ; nMuertos = 0
 
 ; Inicio del bucle 
-xor edx, edx				; i = 0 
+xor edx, edx                ; i = 0 
 
 inicio_bucle: 
-cmp esi, edx				; 
-je fin_bucle				; Si i >= nElementos, fin del bucle. 		
+cmp esi, edx                
+je fin_bucle                ; Si i >= nElementos, fin del bucle.        
 
 ; Cuerpo del bucle for 
 mov ah, [ecx + edx]
-mov al ,[edi + edx]			; Copiamos solucion[i] y propuesta[i] a al y ah 
-cmp al,ah					; if (propuesta[i] == solucion[i])
-je tenemos_muerto			; Salto para acumular muertos si los hay 
-jmp siguiente				; Siguiente si no hay muertos. 
+mov al, [edi + edx]         ; Copiamos solucion[i] y propuesta[i] a al y ah 
+cmp al,ah                   ; if (propuesta[i] == solucion[i])
+je tenemos_muerto           ; Salto para acumular muertos si los hay 
+jmp siguiente               ; Siguiente si no hay muertos. 
 
 tenemos_muerto:
-inc DWORD PTR [ebp -4]		; nMuertos++
+inc DWORD PTR [ebp -4]      ; nMuertos++
 
 siguiente: 
-inc edx						; Incrementamos i (i++) 
-jmp inicio_bucle			; Volvemos al inicio del bucle
+inc edx                     ; Incrementamos i (i++) 
+jmp inicio_bucle            ; Volvemos al inicio del bucle
 
 fin_bucle:
-mov eax, [ebp -4]			; eax = nMuertos 
+mov eax, [ebp -4]           ; eax = nMuertos 
 
-pop	esi
+pop esi
 pop edx 
 pop edi 
-pop ecx						; Restauracion de registros 
+pop ecx                     ; Restauracion de registros 
 
-mov esp, ebp				; Eliminamos variable local 
+mov esp, ebp                ; Eliminamos variable local 
 
-pop ebp						; Epílogo 
+pop ebp                     ; Epílogo 
 ret
 
 calculaNMuertosASM ENDP
